48e5ecaf50359ad1c33922c5f8072fa50c316081
jiro <gitromaru666>
2017-06-09 04:49:34 +0900

    update

--- cgi-bin/converter.py
+++ cgi-bin/converter.py
@@ -7,34 +7,34 @@ print(u"""\
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-	<meta charset="UTF-8">
-	<title>Text Converter</title>
-	<script src="/script/common/util/timer.js"></script>
+    <meta charset="UTF-8">
+    <title>Text Converter</title>
+    <script src="/script/common/util/timer.js"></script>
 </head>
 <body>
-	<form method="post">
-		<h1>log.csvのテキストを配置してください。</h1>
-		<div id="timer" style="font-size: 20px;"></div>
-		<hr>
-		<aside>先頭に定数を定義していない行のみ貼り付けてください。</aside>
-		<textarea id="area" name="oldtext" cols="175" rows="20" autofocus></textarea>
-		<br>
-		<input name="input" type="submit">
-	</form>
-""")
+    <form method="post">
+        <h1>log.csvのテキストを配置してください。</h1>
+        <div id="timer" style="font-size: 20px;"></div>
+        <hr>
+        <aside>先頭に定数を定義していない行のみ貼り付けてください。</aside>
+        <textarea id="area" name="oldtext" cols="175" rows="20" autofocus></textarea>
+        <br>
+        <input name="input" type="submit">
+    </form>
+    """)
 
 import cgi, cgitb
 form = cgi.FieldStorage()
 if form.getvalue("oldtext"):
-	val = form.getvalue("oldtext")
-	from low2up import low2up
-	newval = "".join([low2up(line) for line in val.split("\n")])
-	print(u"""
-			<br>
-			<textarea cols="175" rows="20">{text}</textarea>
-			""".format(text=newval))
+    val = form.getvalue("oldtext")
+    from low2up import low2up
+    newval = "".join([low2up(line) for line in val.split("\n")])
+    print(u"""
+            <br>
+            <textarea cols="175" rows="20">{text}</textarea>
+            """.format(text=newval))
 else:
-	print("please enter text to textarea.")
+    print("please enter text to textarea.")
 #cgitb.enable(display=0, logdir="/log")
 
 
--- cgiserver.py
+++
@@ -1,7 +0,0 @@
-import http.server
-
-server_address = ("", 8080)
-handler = http.server.CGIHTTPRequestHandler
-server = http.server.HTTPServer(server_address, handler)
-server.serve_forever()
-
